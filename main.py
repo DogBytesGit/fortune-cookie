53fd871875e39a4f1198526c2779492e9efc0228
John Sirois <JPSirois@verizon.net>
2017-01-22 20:56:07 -0500

    Added 'another cookie' button

--- main.py
+++ main.py
@@ -17,11 +17,34 @@
 import webapp2
 import random
 
+def getRandomFortune():
+    #list of possible fortunes
+    fortunes = [
+        "Your fortune is in another cookie",
+        "Consider eating more fortune cookies",
+        "No one can escape the grip of the Python"
+        ]
+    index = random.randint(0, 2)
+    return fortunes[index]
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        lucky_number = random.randint(1, 100)
-        self.response.write('Your lucky number: ' + str(lucky_number))
+        header = "<h1>Fortune Cookie</h1>"
 
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = '<p>' + fortune_sentence + '</p>'
+        
+        lucky_number = random.randint(1, 100)        
+        number_sentence = 'Your lucky number: ' + "<strong>" + str(lucky_number) + "</strong>"
+        number_paragraph = '<p>' + number_sentence + '</p>'
+
+        cookie_again_button = "<a href='.'><button>Another cookie plz!</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
+        
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
